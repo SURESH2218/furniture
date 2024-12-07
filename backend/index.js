e4ceca69d48b44033eba0de00c6dc997473442e4
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-07 14:12:35 +0530

    allowing the requests from the frontend and try catch --  connecting to the db

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,40 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "process.env.CORS_ORIGIN",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "Wood Furniture Backend Running" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Successfully connected to the PostgreSQL database");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("PostgreSQL connection failed", error);
+  }
+};
+
+connectDB();
