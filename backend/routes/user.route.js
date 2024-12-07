9d080868d7e9c74b792203bc988f65f00a4a194f
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-07 22:18:37 +0530

    logout route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import userRegister, { userLogin } from "../controllers/user.controller.js";
+import userRegister, {
+  logoutUser,
+  userLogin,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegister);
 router.route("/login").get(userLogin);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
