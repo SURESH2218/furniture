d65ad1102760eefe8404d14e07a9c695cf8295a5
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-07 16:30:01 +0530

    modified asyncHandler

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,20 +1,7 @@
-const asyncHandler = (fn) => {
-  return async (req, res, next) => {
-    try {
-      await fn(req, res, next);
-    } catch (error) {
-      res.status(error.status || 500).json({
-        success: false,
-        messsage: error.messsage,
-      });
-    }
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
 export default asyncHandler;
-
-// const asyncHandler = (requestHandler) => {
-//     return (req, res, next) => {
-//       Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//     };
-//   };
