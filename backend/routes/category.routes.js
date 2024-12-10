01951e1e42ac43cf04163c4f7d67b26b21326c6d
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-10 19:07:57 +0530

    admin end point

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import {
+  createCategory,
+  getCategories,
+} from "../controllers/category.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../controllers/admin.controller.js";
 
 const router = Router();
 
-router.route("/create-category").post(createCategory)
+router
+  .route("/create-category")
+  .post(verifyJWT, authorizeRoles("ADMIN"), createCategory);
+router.route("/get-categories").get(getCategories);
