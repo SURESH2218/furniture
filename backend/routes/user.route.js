3081c30d41ceb0d1b8e37a590ae3d0f9ebe69147
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-10 15:43:03 +0530

    updateProfile route--->

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import userRegister, {
   logoutUser,
+  updateProfile,
   userLogin,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -10,5 +11,6 @@ const router = Router();
 router.route("/register").post(userRegister);
 router.route("/login").get(userLogin);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/update").post(verifyJWT, updateProfile);
 
 export default router;
