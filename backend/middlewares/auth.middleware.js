599c662ac50eaa121d0412dae0e517445868fc61
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-12 12:49:24 +0530

    refreshAccessToken function for the user if the accesstoken got expired then using refresh token we will refresh the accesstoken for the particular user

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import prisma from "../prisma/client.js";
+import { generateAccessToken } from "../models/User.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
@@ -43,5 +44,46 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       401,
       error?.message || "Invalid Access Token: Token verification failed"
     );
+  }
+});
+
+export const refreshAccessToken = asyncHandler(async (req, res) => {
+  const refreshToken = req.cookies?.refreshToken;
+
+  if (!refreshToken) {
+    throw ApiError(401, "Refresh token is missing");
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded._id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        role: true,
+        refreshToken: true,
+      },
+    });
+
+    if (!user || user.refreshToken !== refreshToken) {
+      throw ApiError(401, "Invalid refresh token");
+    }
+
+    const newAccessToken = await generateAccessToken(user);
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", newAccessToken, cookieOptions)
+      .json(newAccessToken);
+  } catch (error) {
+    throw ApiError(401, "Failed to refresh access token");
   }
 });
