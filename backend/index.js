1b0ca4571fc602c5648a0255f4ad03f223b1a8aa
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-17 19:35:31 +0530

    small fix

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "process.env.CORS_ORIGIN",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
