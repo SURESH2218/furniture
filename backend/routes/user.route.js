1569e375ed2ea2341b2bb0873fdf12048068bc83
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-19 00:12:30 +0530

    giving the user to the frontend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -17,7 +17,7 @@ router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/check-auth").get(verifyJWT, (req, res) => {
-  res.status(200).json({ message: "User is authenticated" });
+  res.status(200).json({ message: "User is authenticated", user: req.user });
 });
 router.route("/refresh-token").get(refreshAccessToken);
 router.route("/update").post(verifyJWT, updateProfile);
