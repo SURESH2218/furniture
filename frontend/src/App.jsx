fbec6363dc53f30f03968c79d45d4a482c4ddddf
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-19 00:16:02 +0530

    routing setup for the login,register and for verifyAuth whether the user exists or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,62 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Shop from './pages/shop';
-import Authentication from './auth/Authentication';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
+
+import { verifyAuth } from './store/slices/authSlice';
+
+import Login from './auth/Login';
+import Register from './auth/Register';
+// import Dashboard from './pages/user/Dashboard';
+
 function App() {
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(verifyAuth());
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/auth' element={<Authentication />} />
-      </Routes>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Routes>
+          <Route
+            path='/login'
+            element={
+              isAuthenticated ? <Navigate to='/home' replace /> : <Login />
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              isAuthenticated ? <Navigate to='/home' replace /> : <Register />
+            }
+          />
+          <Route
+            path='/home'
+            element={
+              isAuthenticated ? <Home /> : <Navigate to='/login' replace />
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <Navigate to={isAuthenticated ? '/home' : '/login'} replace />
+            }
+          />
+          <Route path='/shop' element={<Shop />} />
+        </Routes>
+      </Router>
+    </PersistGate>
   );
 }
 
