810d3b28992fd78745d2553cd3b5e3cc261bcaf9
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-12-20 21:00:30 +0530

    products listing page and product details page

---
+++ frontend/src/pages/public/ProductDetails.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+const API_URL = import.meta.env.VITE_BACKEND_URL;
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/product/${id}`);
+        setProduct(data.data);
+      } catch (error) {
+        console.error('Error fetching product details:', error);
+      }
+    };
+
+    fetchProductDetails();
+  }, [id]);
+
+  console.log(product);
+
+  if (!product) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      <p>Stock Quantity: {product.stockQuantity}</p>
+
+      {product?.images?.map((image) => (
+        <div key={image.id}>
+          <img src={image.imageUrl} alt='Product Image' />
+          <p>Is Primary: {image.isPrimary ? 'Yes' : 'No'}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductDetails;
---
+++ frontend/src/pages/public/ProductLIst.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import {
+  setProducts,
+  setLoading,
+  setError,
+} from '../../store/slices/productSlice';
+import { useNavigate } from 'react-router';
+
+const API_URL = import.meta.env.VITE_BACKEND_URL;
+const ProductList = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { products, isLoading, error } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      dispatch(setLoading(true));
+      try {
+        const { data } = await axios.get(`${API_URL}/product/products`);
+        dispatch(setProducts(data.data));
+        dispatch(setLoading(false));
+      } catch (err) {
+        dispatch(setError(err.message || 'Failed to fetch products'));
+      }
+    };
+
+    fetchProducts();
+  }, [dispatch]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div>
+      <h1 className='text-center py-2'>Products List</h1>
+      <div className='rounded-md bg-slate-950 z-40 w-[90%] h-full border mx-auto'>
+        <ul className='grid grid-cols-3 gap-4 p-2'>
+          {products?.map((product) => (
+            <li
+              key={product.id}
+              className='bg-white text-black p-3 border-2 rounded-md'
+              onClick={() => navigate(`/product/${product.id}`)}
+            >
+              <p>{product.name}</p>
+              <p>{product.description}</p>
+              {product?.images?.map((image) => (
+                <div key={image.id}>
+                  <img
+                    src={image.imageUrl}
+                    alt='Product image'
+                    className='p-3 border-black'
+                  />
+                  <p>Is Primary: {image.isPrimary ? 'Yes' : 'No'}</p>
+                </div>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
